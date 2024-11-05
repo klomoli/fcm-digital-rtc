75f807bfc408dbd45a58f38dd287b2e570ff0ba0
Ismael Morales <klomoli@gmail.com>
2024-11-05 18:26:36 +0100

    Fix: Added models folder and created gemfile to add rspec

--- main.rb
+++ main.rb
@@ -1,13 +1,13 @@
-require_relative 'segment'
-require_relative 'trip'
-require_relative 'itinerary'
+require_relative 'models/segment'
+require_relative 'models/trip'
+require_relative 'models/itinerary'
 require_relative 'services/application_service'
 require_relative 'services/file_reader_reservations_service'
 require_relative 'services/segment_parser_service'
 
 reservations = FileReaderReservationsService.call('input.txt')
-segments = SegmentParserService.call(reservations)
+segments = SegmentParserService.call(reservations, 'SEGMENT:')
 
 segments.each do |segment|
-    puts segment.to_s
+  puts segment.to_s
 end
